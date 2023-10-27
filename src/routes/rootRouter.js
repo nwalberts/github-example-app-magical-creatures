56fc276e4a84c06803b19478a1debbfb2d35ab63
Nick Alberts <nwalberts5@gmail.com>
2023-10-27 16:15:34 -0400

    remove comments

--- src/routes/rootRouter.js
+++ src/routes/rootRouter.js
@@ -2,7 +2,6 @@ import express from "express"
 
 const rootRouter = new express.Router()
 
-// if (req.verb === GET && req.path === "/")
 rootRouter.get('/', (req, res) => {
   res.send('Hello from the backend')
 })
@@ -11,24 +10,4 @@ rootRouter.get("/magical-creatures", (req, res) => {
   res.send("This is a list of magical creatures: dragon, unicorn, phoenix")
 })
 
-
-
-// rootRouter.get('/podcasts', (req, res) => {
-//   let podcasts = [
-//     'The Daily',
-//     'MBMBAM',
-//     'Armchair Expert',
-//     'This American Life',
-//     'The Besties',
-//   ]
-
-//   res.render('podcast-index', { podcasts: podcasts })
-// })
-
-// rootRouter.use("/magical-creatures", magicalCreaturesRouter)
-
 export default rootRouter
-
-
-
-
